317de1717acb0920325f01b6bc128745b94c2314
dja322 <127354122+dja322@users.noreply.github.com>
2024-06-12 17:38:56 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+//gets standard Calculator page
 app.get("/", function(req, res){
     console.log("Hello world");
     res.sendFile(__dirname + "/index.html");
 });
 
-
+//returns calculator results
 app.post("/", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
@@ -27,12 +28,13 @@ app.post("/", function(req, res) {
     res.send("The result is " + sum);
 });
 
-//http://localhost:3000/bmiCalculator
+//gets bmi Calculator page
 app.get("/bmicalculator", function(req, res){
     console.log("BMI page");
     res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
+//returns bmi calculator
 app.post("/bmicalculator", function(req, res) {
     var weight = Number(req.body.weight);
     var height = Number(req.body.height);
@@ -43,6 +45,7 @@ app.post("/bmicalculator", function(req, res) {
     res.send("Your BMI is " + bmi.toFixed(2));
 });
 
+//Says which port to listen to
 app.listen(3000, function(){
   console.log("Port on 3000");
 });
